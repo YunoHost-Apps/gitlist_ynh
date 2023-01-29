7f7f1d09ff1239b4097ef4086f3e684ce27e9fe3
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-01-29 18:08:32 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,7 +11,7 @@ YNH_PHP_VERSION=8.1
 php_dependencies="php$YNH_PHP_VERSION-fpm"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="$php_dependencies"
+pkg_dependencies="git $php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
